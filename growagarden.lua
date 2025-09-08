1b120b9259b1530848d02883aa2cfcd433088e90
Alexander Haines <wuhazu18@gmail.com>
2025-09-08 09:29:18 +0200

    Create growagarden.lua

---
+++ growagarden.lua
@@ -0,0 +1,17 @@
+local Starlight = loadstring(game:HttpGet("https://raw.nebulasoftworks.xyz/starlight"))()  
+
+local NebulaIcons = loadstring(game:HttpGet("https://raw.nebulasoftworks.xyz/nebula-icon-library-loader"))()
+local Window = Starlight:CreateWindow({
+    Name = "Grow a Garden",
+    Subtitle = "v1.0",
+    Icon = 123456789,
+
+    LoadingSettings = {
+        Title = "Grow a Garden",
+        Subtitle = "Most popular & best scripts for Grow a Garden",
+    },
+
+    ConfigurationSettings = {
+        FolderName = "gag"
+    },
+})
