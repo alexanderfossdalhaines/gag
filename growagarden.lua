8d82bfd1a4f721f4d27c4f1817f304047f393778
Chariacthus <wuhazu18@gmail.com>
2025-09-08 19:46:38 +0200

    Update growagarden.lua

--- growagarden.lua
+++ growagarden.lua
@@ -132,6 +132,48 @@ Groupbox:CreateButton({
     end,
 }, "INDEX")
 
+Groupbox:CreateParagraph({
+    Name = "Hakariq",
+    Content = "Auto-plant, auto-harvest, auto-sell, and anti-AFK, Keyless Supports all Executors.",
+}, "INDEX")
+
+Groupbox:CreateButton({
+    Name = "Load",
+    Icon = NebulaIcons:GetIcon("bolt", "Material"), -- Rocket for launch
+    Callback = function()
+        Starlight:Notification({
+            Title = "Fetching Script",
+            Icon = NebulaIcons:GetIcon("sparkle", "Material"),
+            Content = "Loading Grow a Garden module...",
+        }, "INDEX")
+
+        task.wait(2)
+        Starlight:Destroy()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/hakariqScripts/Roslina/refs/heads/main/Ro"))()
+    end,
+}, "INDEX")
+
+Groupbox:CreateParagraph({
+    Name = "Depso",
+    Content = "Auto-farm, auto-plant, and auto-sell, Keyless Supports all Executors.",
+}, "INDEX")
+
+Groupbox:CreateButton({
+    Name = "Load",
+    Icon = NebulaIcons:GetIcon("bolt", "Material"), -- Rocket for launch
+    Callback = function()
+        Starlight:Notification({
+            Title = "Fetching Script",
+            Icon = NebulaIcons:GetIcon("sparkle", "Material"),
+            Content = "Loading Grow a Garden module...",
+        }, "INDEX")
+
+        task.wait(2)
+        Starlight:Destroy()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/depthso/Grow-a-Garden/refs/heads/main/autofarm.lua"))()
+    end,
+}, "INDEX")
+
 local Universal = TabSection:CreateTab({
     Name = "Universal Scripts",
     Icon = NebulaIcons:GetIcon("public", "Material"), -- Guaranteed gaming icon
@@ -223,6 +265,38 @@ loadstring(game:HttpGet('https://sirius.menu/script'))()
     end,
 }, "INDEX")
 
+local Button = Groupbox:CreateButton({
+    Name = "UNC Test",
+    Icon = NebulaIcons:GetIcon('public', 'Material'),
+    Style = 1,
+    Tooltip = "Load",
+    Callback = function()
+            local Notifications = Starlight:Notification({
+    Title = "Loading..",
+    Icon = NebulaIcons:GetIcon('sparkle', 'Material'),
+    Content = "Press F9 to see your executors results.",
+}, "INDEX")
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Chariacthus/starlight/refs/heads/main/unc%20test.lua'))()
+            
+    end,
+}, "INDEX")
+
+local Button = Groupbox:CreateButton({
+    Name = "sUNC Test",
+    Icon = NebulaIcons:GetIcon('public', 'Material'),
+    Style = 1,
+    Tooltip = "Load",
+    Callback = function()
+            local Notifications = Starlight:Notification({
+    Title = "Loading..",
+    Icon = NebulaIcons:GetIcon('sparkle', 'Material'),
+    Content = "Press F9 to see your executors results.",
+}, "INDEX")
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Chariacthus/starlight/refs/heads/main/sunc.lua'))()
+            
+    end,
+}, "INDEX")
+
 local Groupbox = Universal:CreateGroupbox({
     Name = "Scripts Explained",
     Column = 2,
@@ -252,3 +326,202 @@ Groupbox:CreateParagraph({
     Name = "Sirius Menu",
     Content = "Sirius Menu is a Roblox script hub that gives players a variety of cheats, like teleporting, Speed Hacks & Script Search.",
 }, "INDEX")
+
+Groupbox:CreateParagraph({
+    Name = "UNC Test",
+    Content = "A UNC test in Roblox checks if an executor supports essential features like `getgenv()`, `loadstring()`, GUI creation, and executor detection to ensure scripts run safely.",
+}, "INDEX")
+
+Groupbox:CreateParagraph({
+    Name = "sUNC Test",
+    Content = "sUNC test is basically a short/streamlined UNC test that quickly checks whether an executor supports essential features (`getgenv()`, `loadstring()`, GUI creation, etc.) so scripts can safely run.",
+}, "INDEX")
+
+local Groupbox = Universal:CreateGroupbox({
+    Name = "FE Scripts",
+    Column = 1,
+}, "INDEX")
+
+local Button = Groupbox:CreateButton({
+    Name = "Fe Punch",
+    Icon = NebulaIcons:GetIcon('public', 'Material'),
+    Style = 2,
+    Tooltip = "Load",
+    Callback = function()
+        Starlight:Notification({
+            Title = "Loading...",
+            Icon = NebulaIcons:GetIcon('sparkle', 'Material'),
+            Content = "You may experience some lag while it loads.",
+        }, "INDEX")
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/0Ben1/fe/main/obf_rf6iQURzu1fqrytcnLBAvW34C9N55kS9g9G3CKz086rC47M6632sEd4ZZYB0AYgV.lua.txt', true))()
+    end,
+}, "INDEX")
+
+local Button = Groupbox:CreateButton({
+    Name = "Fe Neko",
+    Icon = NebulaIcons:GetIcon('public', 'Material'),
+    Style = 2,
+    Tooltip = "Load",
+    Callback = function()
+        Starlight:Notification({
+            Title = "Loading...",
+            Icon = NebulaIcons:GetIcon('sparkle', 'Material'),
+            Content = "You may experience some lag while it loads.",
+        }, "INDEX")
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Gazer-Ha/Neko-v1/main/Extremely%20Broken"))()
+    end,
+}, "INDEX")
+
+local Button = Groupbox:CreateButton({
+    Name = "Fe Ender",
+    Icon = NebulaIcons:GetIcon('public', 'Material'),
+    Style = 2,
+    Tooltip = "Load",
+    Callback = function()
+        Starlight:Notification({
+            Title = "Loading...",
+            Icon = NebulaIcons:GetIcon('sparkle', 'Material'),
+            Content = "You may experience some lag while it loads.",
+        }, "INDEX")
+        loadstring(game:HttpGet("https://pastebin.com/raw/9BtJhHrp", true))()
+    end,
+}, "INDEX")
+
+local Button = Groupbox:CreateButton({
+    Name = "Fe Glios",
+    Icon = NebulaIcons:GetIcon('public', 'Material'),
+    Style = 2,
+    Tooltip = "Load",
+    Callback = function()
+        Starlight:Notification({
+            Title = "Loading...",
+            Icon = NebulaIcons:GetIcon('sparkle', 'Material'),
+            Content = "You may experience some lag while it loads.",
+        }, "INDEX")
+        loadstring(game:HttpGet('https://glot.io/snippets/gua2ntmbdm/raw/main.lua', true))()
+    end,
+}, "INDEX")
+
+local Button = Groupbox:CreateButton({
+    Name = "Fe Gale Fighter",
+    Icon = NebulaIcons:GetIcon('public', 'Material'),
+    Style = 2,
+    Tooltip = "Load",
+    Callback = function()
+        Starlight:Notification({
+            Title = "Loading...",
+            Icon = NebulaIcons:GetIcon('sparkle', 'Material'),
+            Content = "You may experience some lag while it loads.",
+        }, "INDEX")
+        loadstring(game:HttpGet("https://pastebin.com/raw/XPGSMEw9"))()
+    end,
+}, "INDEX")
+
+local Button = Groupbox:CreateButton({
+    Name = "Fe Griddy",
+    Icon = NebulaIcons:GetIcon('public', 'Material'),
+    Style = 2,
+    Tooltip = "Load",
+    Callback = function()
+        Starlight:Notification({
+            Title = "Loading...",
+            Icon = NebulaIcons:GetIcon('sparkle', 'Material'),
+            Content = "You may experience some lag while it loads.",
+        }, "INDEX")
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/MelonsStuff/hub/main/Griddy.txt"))()
+    end,
+}, "INDEX")
+
+local Button = Groupbox:CreateButton({
+    Name = "Fe KJ",
+    Icon = NebulaIcons:GetIcon('public', 'Material'),
+    Style = 2,
+    Tooltip = "Load",
+    Callback = function()
+        Starlight:Notification({
+            Title = "Loading...",
+            Icon = NebulaIcons:GetIcon('sparkle', 'Material'),
+            Content = "You may experience some lag while it loads.",
+        }, "INDEX")
+        loadstring(game:HttpGet("https://pastefy.app/sdAujywd/raw"))()
+    end,
+}, "INDEX")
+
+local Button = Groupbox:CreateButton({
+    Name = "Fe Caducus",
+    Icon = NebulaIcons:GetIcon('public', 'Material'),
+    Style = 2,
+    Tooltip = "Load",
+    Callback = function()
+        Starlight:Notification({
+            Title = "Loading...",
+            Icon = NebulaIcons:GetIcon('sparkle', 'Material'),
+            Content = "You may experience some lag while it loads.",
+        }, "INDEX")
+        loadstring(game:HttpGet("https://pastebin.com/raw/LDL9AyQ4"))()
+    end,
+}, "INDEX")
+
+local Button = Groupbox:CreateButton({
+    Name = "Fe Sonic",
+    Icon = NebulaIcons:GetIcon('public', 'Material'),
+    Style = 2,
+    Tooltip = "Load",
+    Callback = function()
+        Starlight:Notification({
+            Title = "Loading...",
+            Icon = NebulaIcons:GetIcon('sparkle', 'Material'),
+            Content = "You may experience some lag while it loads.",
+        }, "INDEX")
+        loadstring(game:HttpGet("https://pastebin.com/raw/uacVtsWe"))()
+    end,
+}, "INDEX")
+
+local Button = Groupbox:CreateButton({
+    Name = "Fe Sad Boy",
+    Icon = NebulaIcons:GetIcon('public', 'Material'),
+    Style = 2,
+    Tooltip = "Load",
+    Callback = function()
+        Starlight:Notification({
+            Title = "Loading...",
+            Icon = NebulaIcons:GetIcon('sparkle', 'Material'),
+            Content = "You may experience some lag while it loads.",
+        }, "INDEX")
+        loadstring(game:HttpGet("https://pastebin.com/raw/hgPJbwF0"))()
+    end,
+}, "INDEX")
+
+-- FE G-Man
+local Button = Groupbox:CreateButton({
+    Name = "Fe G-Man",
+    Icon = NebulaIcons:GetIcon('public', 'Material'),
+    Style = 2,
+    Tooltip = "Load",
+    Callback = function()
+        _G.clientsidedeffect = true
+        Starlight:Notification({
+            Title = "Loading...",
+            Icon = NebulaIcons:GetIcon('sparkle', 'Material'),
+            Content = "You may experience some lag while it loads.",
+        }, "INDEX")
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/randomstring0/Qwerty/refs/heads/main/qwerty18.lua"))()
+    end,
+}, "INDEX")
+
+-- FE Car
+local Button = Groupbox:CreateButton({
+    Name = "Fe Car",
+    Icon = NebulaIcons:GetIcon('public', 'Material'),
+    Style = 2,
+    Tooltip = "Load",
+    Callback = function()
+        Starlight:Notification({
+            Title = "Loading...",
+            Icon = NebulaIcons:GetIcon('sparkle', 'Material'),
+            Content = "You may experience some lag while it loads.",
+        }, "INDEX")
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/AlexCr4sh/FeScripts/main/FeCarScript.lua", true))()
+    end,
+}, "INDEX")
+
