0ac56628f79568b440b613a5780b2c2bcd355af0
Alexander Haines <wuhazu18@gmail.com>
2025-09-08 11:27:57 +0200

    Update growagarden.lua

--- growagarden.lua
+++ growagarden.lua
@@ -15,3 +15,43 @@ local Window = Starlight:CreateWindow({
         FolderName = "gag"
     },
 })
+
+local Main = TabSection:CreateTab({ Name = "Main", Icon = Icons:GetIcon('view_in_ar','Material'), Columns = 2 }, "INDEX")
+local Game = TabSection:CreateTab({ Name = "Game Scripts", Icon = Icons:GetIcon('view_in_ar','Material'), Columns = 2 }, "INDEX")
+
+local Groupbox = Game:CreateGroupbox({ Name = "Popular Scripts", Column = 1 }, "INDEX")
+
+local Button = Groupbox:CreateButton({
+    Name = "ZapHub",
+    Icon = NebulaIcons:GetIcon('check', 'Material'),
+    Callback = function()
+          Starlight:Notification({
+    Title = "Fetching Script",
+    Icon = Icons:GetIcon('sparkle','Material'),
+    Content = "Loading Grow a Garden module...",
+}, "INDEX")
+      wait(2)
+        Starlight:Destroy()
+loadstring(game:HttpGet('https://zaphub.xyz/Exec'))()
+
+    end,
+}, "INDEX")
+
+local Button = Groupbox:CreateButton({
+    Name = "Koronis",
+    Icon = NebulaIcons:GetIcon('check', 'Material'),
+    Callback = function()
+          Starlight:Notification({
+    Title = "Fetching Script",
+    Icon = Icons:GetIcon('sparkle','Material'),
+    Content = "Loading Grow a Garden module...",
+}, "INDEX")
+      wait(2)
+        Starlight:Destroy()
+loadstring(game:HttpGet("https://koronis.xyz/hub.lua"))()
+
+    end,
+}, "INDEX")
+local Label = Groupbox:CreateLabel({
+    Name = "Koronis is not supported by Solara or Xeno."
+}, "INDEX")
