7570707664c8ca4e0de2df3ae4ff1fbe77c240e4
Alexander Haines <wuhazu18@gmail.com>
2025-09-08 17:28:55 +0200

    Update growagarden.lua

--- growagarden.lua
+++ growagarden.lua
@@ -145,12 +145,110 @@ local Groupbox = Universal:CreateGroupbox({
 
 local Button = Groupbox:CreateButton({
     Name = "Infinite Yield",
-    Icon = NebulaIcons:GetIcon('check', 'Material'),
+    Icon = NebulaIcons:GetIcon('public', 'Material'),
     Style = 2,
+    Tooltip = "Load",
     Callback = function()
+            local Notifications = Starlight:Notification({
+    Title = "Loading..",
+    Icon = NebulaIcons:GetIcon('sparkle', 'Material'),
+    Content = "You may experience some lag while it loads.",
+}, "INDEX")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
             
     end,
 }, "INDEX")
 
+local Button = Groupbox:CreateButton({
+    Name = "Dark Dex V5",
+    Icon = NebulaIcons:GetIcon('public', 'Material'),
+    Style = 2,
+    Tooltip = "Load",
+    Callback = function()
+            local Notifications = Starlight:Notification({
+    Title = "Loading..",
+    Icon = NebulaIcons:GetIcon('sparkle', 'Material'),
+    Content = "You may experience some lag while it loads.",
+}, "INDEX")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/luastate/dex-v5/refs/heads/main/DarkDEX-V5"))()
+            
+    end,
+}, "INDEX")
 
+local Button = Groupbox:CreateButton({
+    Name = "AFEM Max",
+    Icon = NebulaIcons:GetIcon('public', 'Material'),
+    Style = 2,
+    Tooltip = "Load",
+    Callback = function()
+            local Notifications = Starlight:Notification({
+    Title = "Loading..",
+    Icon = NebulaIcons:GetIcon('sparkle', 'Material'),
+    Content = "You may experience some lag while it loads.",
+}, "INDEX")
+loadstring(game:HttpGet("https://yarhm.mhi.im/scr?channel=afemmax"))()
+            
+    end,
+}, "INDEX")
+
+local Button = Groupbox:CreateButton({
+    Name = "Remote Spy",
+    Icon = NebulaIcons:GetIcon('public', 'Material'),
+    Style = 2,
+    Tooltip = "Load",
+    Callback = function()
+            local Notifications = Starlight:Notification({
+    Title = "Loading..",
+    Icon = NebulaIcons:GetIcon('sparkle', 'Material'),
+    Content = "You may experience some lag while it loads.",
+}, "INDEX")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/InfernusScripts/Ketamine/refs/heads/main/Ketamine.lua"))()
+            
+    end,
+}, "INDEX")
+
+local Button = Groupbox:CreateButton({
+    Name = "Sirius Menu",
+    Icon = NebulaIcons:GetIcon('public', 'Material'),
+    Style = 2,
+    Tooltip = "Load",
+    Callback = function()
+            local Notifications = Starlight:Notification({
+    Title = "Loading..",
+    Icon = NebulaIcons:GetIcon('sparkle', 'Material'),
+    Content = "You may experience some lag while it loads.",
+}, "INDEX")
+loadstring(game:HttpGet('https://sirius.menu/script'))()
+            
+    end,
+}, "INDEX")
+
+local Groupbox = Universal:CreateGroupbox({
+    Name = "Scripts Explained",
+    Column = 2,
+}, "INDEX")
+
+Groupbox:CreateParagraph({
+    Name = "Infinite Yield",
+    Content = "Infinite Yield is a Roblox admin script that lets you run commands to cheat the game, like teleporting, giving items, or trolling players.",
+}, "INDEX")
+
+Groupbox:CreateParagraph({
+    Name = "Dark Dex",
+    Content = "Dark Dex lets you edit client-side game elements and view scripts.",
+}, "INDEX")
+
+Groupbox:CreateParagraph({
+    Name = "AFEM Max",
+    Content = "AFEM Max is a Roblox script that enhances emotes and animations.",
+}, "INDEX")
+
+Groupbox:CreateParagraph({
+    Name = "Remote Spy",
+    Content = "Remote Spy is a Roblox tool that lets you view and trigger RemoteEvents and RemoteFunctions between the client and server.",
+}, "INDEX")
+
+Groupbox:CreateParagraph({
+    Name = "Sirius Menu",
+    Content = "Sirius Menu is a Roblox script hub that gives players a variety of cheats, like teleporting, Speed Hacks & Script Search.",
+}, "INDEX")
